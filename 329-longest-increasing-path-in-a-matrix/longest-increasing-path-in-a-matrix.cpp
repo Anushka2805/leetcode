30de01f798928c6b5620bdd7bcb0bbb513410004
Anushka Aggarwal <anushkaggarwal2005@gmail.com>
2026-01-23 15:32:46 +0530

    Time: 4 ms (91.63%) | Memory: 20.9 MB (61.86%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
@@ -23,7 +23,7 @@ public:
     int longestIncreasingPath(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+        vector<vector<int>> dp(n , vector<int>(m , -1));
         int ans = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
