1a25fad1866ff9e92cc7b4f61ca945337d073700
Anushka Aggarwal <anushkaggarwal2005@gmail.com>
2026-01-28 13:35:01 +0530

    Time: 47 ms (20.73%) | Memory: 32.3 MB (37.21%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.cpp
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
+//tc = O(n logn)
     bool isNStraightHand(vector<int>& hand, int groupSize) {
         if (hand.size() % groupSize != 0) {
             return false;
         }
+        //(num,freq)
         map<int, int> freq;
         for (int& h : hand) {
-            freq[h]++;
+            freq[h]++; 
         }
         while (!freq.empty()) {
             int curr = freq.begin()->first;
+            //checking consec nums
             for (int i = 0; i < groupSize; i++) {
                 if (freq[curr + i] == 0) {
                     return false;
                 }
                 freq[curr + i]--;
-                if (freq[curr + i] < 1) {
+                //erasing num if freq becomes 0
+                if (freq[curr + i] == 0) {
                     freq.erase(curr + i);
                 }
             }
