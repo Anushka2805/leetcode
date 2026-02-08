87c3525d7de0f976c96ab2187dcb5778f18a6b33
Anushka Aggarwal <anushkaggarwal2005@gmail.com>
2026-02-08 19:11:02 +0530

    Time: 3 ms (14.49%) | Memory: 9.1 MB (80.49%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int n;
+    int sign = 1;
+    long long f(string& s, int i, long long num) {
+        if (i >= n || s[i] < '0' || s[i] > '9') {
+            return num;
+        }
+        int dig = s[i] - '0';
+        num = num * 10 + dig;
+
+        if (sign == 1 && num > INT_MAX) {
+            return INT_MAX;
+        }
+        if (sign == -1 && -num < INT_MIN) {
+            return (long long)INT_MAX + 1;
+        }
+        return f(s, i + 1, num);
+    }
+    int myAtoi(string s) {
+        n = s.size();
+        int i = 0;
+        while (i < n && s[i] == ' ') {
+            i++;
+        }
+        if (i < n && (s[i] == '+' || s[i] == '-')) {
+            if (s[i] == '-') {
+                sign = -1;
+            }
+            i++;
+        }
+        long long num = f(s, i, 0);
+        num = num * sign;
+        if (num > INT_MAX) {
+            return INT_MAX;
+        }
+        if (num < INT_MIN) {
+            return INT_MIN;
+        }
+        return (int)num;
+    }
+};
